0ba822c8da06afd36e97adc576cccb48254f65d9
JonathamCarvalho <jonatham.carvalho@gmail.com>
2026-01-31 16:55:31 -0300

    Refactor ship placement and add skill matrices
    Updated ship placement and added skill matrices for game mechanics.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TABULEIRO 10
 #define AGUA 0
 #define TAMNAVIO 3
 #define NAVIO 3
 #define LINHA 10
+#define AREA_AFETADA 5
+#define HABILIDADE 5
 
 int main()
 {
@@ -28,38 +31,90 @@ int main()
             tabuleiro[i][j] = AGUA;
         }
     }
-    // 3. Definir posições dos navios na posição horizontal, vertical e diagonal
+    // 3. Navios no tabuleiro
 
-    // Navio horizontal
-    int linhaH = 1, colunaH = 2;
-    int linhaI = 2, colunaI = 8;
+    int linhaH = 0, colunaH = 5;
+    int linhaJ = 7, colunaJ = 9;
+
     for (int j = 0; j < TAMNAVIO; j++)
-    {
         tabuleiro[linhaH][colunaH + j] = NAVIO;
-    }
-    for (int j = 0; j < TAMNAVIO; j++)
-    {
-        tabuleiro[linhaI + j][colunaI] = NAVIO;
-    }
 
-    // Navio diagonal para baixo à direita e para baixo à esquerda
-    int linhaD = 3, colunaD = 3;
-    int linhaF = 7, colunaF = 5;
+    for (int j = 0; j < TAMNAVIO; j++)
+        tabuleiro[linhaJ + j][colunaJ] = NAVIO;
+
+    int linhaD = 7, colunaD = 5;
+    int linhaF = 7, colunaF = 2;
 
     for (int i = 0; i < TAMNAVIO; i++)
-    {
         if (tabuleiro[linhaD + i][colunaD + i] == AGUA)
-        {
             tabuleiro[linhaD + i][colunaD + i] = NAVIO;
+
+    for (int j = 0; j < TAMNAVIO; j++)
+        if ((linhaF + j) < TABULEIRO && (colunaF - j) >= 0)
+            tabuleiro[linhaF + j][colunaF - j] = NAVIO;
+
+    // 4. Matrizes de habilidade
+
+    int cone[HABILIDADE][HABILIDADE];
+    int cruz[HABILIDADE][HABILIDADE];
+    int octaedro[HABILIDADE][HABILIDADE];
+
+    int centro = HABILIDADE / 2;
+
+    // Cone (forma triangular)
+    for (int i = 0; i < HABILIDADE; i++)
+    {
+        for (int j = 0; j < HABILIDADE; j++)
+        {
+            if (i <= centro && j >= centro - i && j <= centro + i)
+                cone[i][j] = 1;
+            else
+                cone[i][j] = 0;
         }
     }
 
-    for (int j = 0; j < TAMNAVIO; j++)
-    {
+    // Cruz (forma de cruz)
+    for (int i = 0; i < HABILIDADE; i++)
+        for (int j = 0; j < HABILIDADE; j++)
+            cruz[i][j] = (i == centro || j == centro) ? 1 : 0;
 
-        if ((linhaF + j) < TABULEIRO && (colunaF - j) >= 0)
+    // octaedro (forma de losango)
+    for (int i = 0; i < HABILIDADE; i++)
+        for (int j = 0; j < HABILIDADE; j++)
+            octaedro[i][j] = (abs(i - centro) + abs(j - centro) <= centro) ? 1 : 0;
+
+    // 4. Aplicar habilidades
+
+    int coneL = 2, coneC = 2;
+    int cruzL = 4, cruzC = 7;
+    int octL = 6, octC = 2;
+
+    for (int i = 0; i < HABILIDADE; i++)
+    {
+        for (int j = 0; j < HABILIDADE; j++)
         {
-            tabuleiro[linhaF + j][colunaF - j] = NAVIO;
+            int l, c;
+
+            // Cone
+            l = coneL + i - centro;
+            c = coneC + j - centro;
+            if (l >= 0 && l < TABULEIRO && c >= 0 && c < TABULEIRO)
+                if (cone[i][j] && tabuleiro[l][c] == AGUA)
+                    tabuleiro[l][c] = AREA_AFETADA;
+
+            // Cruz
+            l = cruzL + i - centro;
+            c = cruzC + j - centro;
+            if (l >= 0 && l < TABULEIRO && c >= 0 && c < TABULEIRO)
+                if (cruz[i][j] && tabuleiro[l][c] == AGUA)
+                    tabuleiro[l][c] = AREA_AFETADA;
+
+            // Octaedro
+            l = octL + i - centro;
+            c = octC + j - centro;
+            if (l >= 0 && l < TABULEIRO && c >= 0 && c < TABULEIRO)
+                if (octaedro[i][j] && tabuleiro[l][c] == AGUA)
+                    tabuleiro[l][c] = AREA_AFETADA;
         }
     }
 
